3b5eae2723a7f64b892830ec365db4a459983fe5
med4h <adrian.meda6@gmail.com>
2024-11-25 21:29:24 -0800

    added DELETE and POST

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -9,6 +9,14 @@ import apiURL from '../api'
 export const App = () => {
   const [pages, setPages] = useState([]);
   const [selectedPage, setSelectedPage] = useState(null);
+  const [isAddingArticle, setIsAddingArticle] = useState(false);
+  const [newPage, SetNewPage] = useState({
+    title: '',
+    content: '', 
+    name: '',
+    email: '',
+    tags: ''
+  })
   
   async function fetchPages() {
     try {
@@ -30,6 +38,61 @@ export const App = () => {
     }
   }
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    SetNewPage(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const articleData = {
+      title: newPage.title,
+      content: newPage.content,
+      name: newPage.email,
+      tags: newPage.tags
+  }
+
+  try {
+    const response = await fetch(`${apiURL}/wiki`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(articleData)
+    });
+  
+    if (response.status === 201) {
+      await fetchPages();
+      setIsAddingArticle(false);
+      SetNewPage({
+        title: '',
+        content: '',
+        name: '',
+        email: '',
+        tags: ''
+      })
+    }} catch(err) {
+      console.log('Oh no an error!', err)
+    }
+
+    const handleDeletepage = async (slug) => {
+      try {
+        const response = await fetch(`${apiURL}/wiki/${slug}`, {
+          method: 'DELETE'
+        });
+        if (response.status === 204) {
+          await fetchPages();
+          setSelectedPage(null);
+        }
+      } catch(err) {
+        console.log('Oh no an error!', err)
+      }
+    }
+
   useEffect(() => {
     fetchPages();
   }, []);
@@ -38,11 +101,28 @@ export const App = () => {
 		<main>
       <h1>WikiVerse</h1>
 			<h2>An interesting 📚</h2>
-      {selectedPage ? ( 
+      <button onClick={() => setIsAddingArticle(true)}>Add Article</button>
+      {isAddingArticle && (
+        <form onSubmit={handleSubmit}>
+          <label> Title: <input type="text" name="title" value={newPage.title} onChange={handleInputChange}/>
+          </label>
+          <label> Content: <textarea name="content" value={newPage.content} onChange={handleInputChange}/>
+          </label>
+          <label> Name: <input type="text" name="name" value={newPage.name} onChange={handleInputChange}/>
+          </label>
+          <label>Email: <input type="email" name="email" value={newPage.email} onChange={handleInputChange}/>
+          </label>
+          <label> Tags:
+            <input type="text" name="tags" value={newPage.tags} onChange={handleInputChange}/>
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      )} : selectedPage ? ( 
         <PageDetails page={selectedPage} goBack={() => setSelectedPage(null)}/>
+          <button onClick={() => handleDeletePage(selectedPage.slug)}>Delete</button>
       ) : (
 			<PagesList pages={pages} onPageClick={fetchPageDetails}/>
-      )} 
+      )
     </main>
   )
-}
+}}
